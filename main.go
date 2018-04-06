98d2b0afb001f4883e834600c99797fc5605467c
Jeremy Echols <jechols@uoregon.edu>
2018-04-06 12:49:58 -0700

    Use machine.Empty in main program

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func main() {
 	var m = machine.New()
 
 	// Destroy All Software initial "flip symbols" example
-	m.AddInstruction("s1", '0', '1', 'R', "s2")
-	m.AddInstruction("s2", '0', '0', 'L', "s3")
-	m.AddInstruction("s3", '1', '0', 'R', "s4")
-	m.AddInstruction("s4", '0', '0', 'L', "s1")
+	m.AddInstruction("s1", machine.Empty, '1', 'R', "s2")
+	m.AddInstruction("s2", machine.Empty, machine.Empty, 'L', "s3")
+	m.AddInstruction("s3", '1', machine.Empty, 'R', "s4")
+	m.AddInstruction("s4", machine.Empty, machine.Empty, 'L', "s1")
 
 	fmt.Println("DAS: Toggle 0->1")
 	fmt.Println("----------------")
@@ -26,24 +26,24 @@ func main() {
 	m = machine.New()
 
 	// Initial state: the problem of 2 + 3
-	m.AddInstruction("s1", '0', '(', 'L', "s2")
-	m.AddInstruction("s2", '0', '1', 'L', "s3")
-	m.AddInstruction("s3", '0', '1', 'L', "s4")
-	m.AddInstruction("s4", '0', '+', 'L', "s5")
-	m.AddInstruction("s5", '0', '1', 'L', "s6")
-	m.AddInstruction("s6", '0', '1', 'L', "s7")
-	m.AddInstruction("s7", '0', '1', 'L', "s8")
-	m.AddInstruction("s8", '0', ')', 'L', "s9")
+	m.AddInstruction("s1", machine.Empty, '(', 'L', "s2")
+	m.AddInstruction("s2", machine.Empty, '1', 'L', "s3")
+	m.AddInstruction("s3", machine.Empty, '1', 'L', "s4")
+	m.AddInstruction("s4", machine.Empty, '+', 'L', "s5")
+	m.AddInstruction("s5", machine.Empty, '1', 'L', "s6")
+	m.AddInstruction("s6", machine.Empty, '1', 'L', "s7")
+	m.AddInstruction("s7", machine.Empty, '1', 'L', "s8")
+	m.AddInstruction("s8", machine.Empty, ')', 'L', "s9")
 
 	// Find the plus
-	m.AddInstruction("s9", '0', '0', 'R', "s9")
+	m.AddInstruction("s9", machine.Empty, machine.Empty, 'R', "s9")
 	m.AddInstruction("s9", '1', '1', 'R', "s9")
 	m.AddInstruction("s9", ')', ')', 'R', "s9")
 
 	// Remove and combine
 	m.AddInstruction("s9", '+', '1', 'L', "s10")
 	m.AddInstruction("s10", '1', '1', 'L', "s10")
-	m.AddInstruction("s10", ')', '0', 'R', "s11")
+	m.AddInstruction("s10", ')', machine.Empty, 'R', "s11")
 	m.AddInstruction("s11", '1', ')', 'R', "done")
 
 	fmt.Println("DAS: Add 2+3")
